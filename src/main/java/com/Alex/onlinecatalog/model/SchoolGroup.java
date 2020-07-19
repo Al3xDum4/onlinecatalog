d0add6c4dc517030fdae662b56e1e54571fcec55
Alex Duma <alex.m.duma@gmail.com>
2020-07-19 09:16:23 +0300

    fix navbar

--- src/main/java/com/Alex/onlinecatalog/model/SchoolGroup.java
+++ src/main/java/com/Alex/onlinecatalog/model/SchoolGroup.java
@@ -21,9 +21,25 @@ public class SchoolGroup {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int groupId;
 
+    /*
+    * avem deja maparea facuta pt ca pareta detinatoare
+    * a relatiei este entitatea Students in tabelul relational
+    * al entitatii fiind gestionata coloana FK.
+    *
+    * In aceasta entitate, prin mappedBy, se creaza maparea bidirectionala a entitatilor,
+    * lista de studenti o folosim pt a obtine informatiile despre studenti in entitatea group.
+    *
+    * cascade - daca un grup este sters, se sterg odata cu el si asocierile studentilor acelui grup,
+    * in cazul de fata aplicabil pe toate operatile de persistenta asupra entitatii
+    */
     @OneToMany(mappedBy = "schoolGroup", cascade = CascadeType.ALL)
     private List<Student> students;
 
+    /*
+    * tabel suplimentar, il numim prin atributul name (group_discipline).
+    * coloana alaturata la entitate este group_id (prin JoinColumn),
+    * iar coloana inversa este discipline_id
+    */
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "group_discipline", joinColumns = @JoinColumn(name = "group_id"),
             inverseJoinColumns = @JoinColumn(name = "discipline_id"))
