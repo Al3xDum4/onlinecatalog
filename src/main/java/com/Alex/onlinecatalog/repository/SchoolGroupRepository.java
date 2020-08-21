182922a5a6efa3cae6da56f19f8e742345cf2a5e
Alex Duma <alex.m.duma@gmail.com>
2020-08-21 21:13:02 +0300

    viewprofessors card 4

--- src/main/java/com/Alex/onlinecatalog/repository/SchoolGroupRepository.java
+++ src/main/java/com/Alex/onlinecatalog/repository/SchoolGroupRepository.java
@@ -18,6 +18,6 @@ public interface SchoolGroupRepository extends JpaRepository<SchoolGroup, Intege
 //    @Query("FROM SchoolGroup s where s.professor.id = :professorId")
 //    Optional<SchoolGroup> findAllProfessorsByGroup(@Param("professorId") Integer professorId);
 
-    @Query("SELECT p FROM Professor p where p.discipline.disciplineName = :groupId")
+    @Query("SELECT s, p FROM SchoolGroup s, Professor p WHERE s.professor.professorId = p.professorId")
     List<Professor> findAllProfessorsByGroup(@Param("groupId") Integer groupId);
 }
